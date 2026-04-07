3d9c0ae8c4b10506393ee8ff10482a9bdfbe95e4
lenainweb <lenainweb@gmail.com>
2026-04-07 02:00:01 +0200

    chore: update requirements

--- requirements/compile.sh
+++ requirements/compile.sh
@@ -3,3 +3,4 @@ set -e
 cd "$(dirname $0)"
 
 pip-compile -q --no-emit-index-url --output-file requirements.txt requirements.in
+pip-compile -q --no-emit-index-url --output-file requirements.android.txt requirements.android.in
